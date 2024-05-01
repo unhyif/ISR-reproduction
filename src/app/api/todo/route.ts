8ee99bb2563e7feeb71018237c33b0acf5d957fd
unhyif <ang21icjh@gmail.com>
2024-05-02 00:26:00 +0900

    set revalidate in router handler

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -2,7 +2,12 @@ import { getRandomInt } from "@/utils";
 
 export async function GET() {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${getRandomInt(1, 200)}`
+    `https://jsonplaceholder.typicode.com/todos/${getRandomInt(1, 200)}`,
+    {
+      next: {
+        revalidate: 2,
+      },
+    }
   );
   const { id } = await res.json();
   return Response.json({
